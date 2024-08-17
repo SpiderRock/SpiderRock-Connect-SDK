442a54875d815c3d3656e421eb8dcb1117eb101e
Jon Dugan <jdugan@spantree.net>
2024-08-16 22:24:11 -0500

    add TimeBoxAnalysis and CacheProbe

---
+++ SpiderStream/_csharp/CacheProbe/Program.cs
@@ -0,0 +1,100 @@
+﻿using SpiderRock.SpiderStream;
+using SpiderRock.SpiderStream.Diagnostics;
+using SpiderRock.SpiderStream.Mbus;
+using SpiderRock.SpiderStream.MLink;
+using System;
+
+namespace CacheProbe;
+
+internal class Program
+{
+    public static string ApiKey;
+    private static void Main(string[] args)
+    {
+        var requestInterval = TimeSpan.FromSeconds(90);
+
+        DateTime now = DateTime.Now;
+        DateTime when;
+        TimeSpan startTimeSpan;
+        
+        if (args.Length == 0)
+        {
+            Console.WriteLine($"{nameof(CacheProbe)}: API Key is required");
+            return;
+        }
+        ApiKey = args[0];
+        string mode = args.Length == 2 ? args[1] : "default";
+        DateTime tomorrow = now.AddDays(1);
+
+
+        switch (mode)
+        {
+            case "zero":
+            case "now":
+                when = now;
+                startTimeSpan = TimeSpan.Zero;
+                break;
+            case "12a":
+                when = new DateTime(tomorrow.Year, tomorrow.Month, tomorrow.Day, 0, 0, 0);
+                startTimeSpan = when - now;
+                break;
+            case "2a":
+                if (now.Hour < 2)
+                {
+                    when = new DateTime(now.Year, now.Month, now.Day, 2, 0, 0);
+                }
+                else
+                {
+                    when = new DateTime(tomorrow.Year, tomorrow.Month, tomorrow.Day, 2, 0, 0);
+                }
+                startTimeSpan = when - now;
+                break;
+            default:
+                DateTime nextHour = now.AddHours(1);
+                when = new DateTime(nextHour.Year, nextHour.Month, nextHour.Day, nextHour.Hour, 0, 0);
+                startTimeSpan = when - now;
+                break;
+        }
+
+
+
+        var periodTimeSpan = TimeSpan.FromHours(1);
+
+        SRTrace.Process.TraceInfo($"Next run will be at {when} and every {periodTimeSpan} after");
+
+        var timer = new System.Threading.Timer((e) => DoCacheRequests(2, requestInterval), null, startTimeSpan, periodTimeSpan);
+
+        SRTrace.Process.TraceInfo($"{nameof(CacheProbe)} running. Hit [enter] to exit.");
+        Console.ReadLine();
+    }
+    private static void DoCacheRequests(int limit, TimeSpan sleepFor)
+    {
+        for (int i = 0; i < limit; i++)
+        {
+            SRTrace.Process.TraceInfo($"{nameof(DoCacheRequests)}: Cache request {i + 1}");
+            DoCacheRequest();
+            if (i < limit - 1)
+            {
+                SRTrace.Process.TraceInfo($"Sleeping for {sleepFor}");
+                System.Threading.Thread.Sleep(sleepFor);
+            }
+        }
+    }
+
+    private static void DoCacheRequest()
+    {
+        SRTrace.Process.TraceInfo($"{nameof(DoCacheRequest)} Start cache request at {DateTime.Now}");
+
+        MessageType[] messageTypes = new MessageType[1];
+        messageTypes[0] = (MessageType)1015; // LiveImpliedQuote
+
+        var _ = new MLinkWebSocketClient(
+            SysEnvironment.Saturn,
+            SysRealm.NMS,
+            messageTypes,
+            "Cache Probe",
+            ApiKey,
+            ""
+        );
+    }
+}
---
+++ SpiderStream/_csharp/TimeBoxAnalysis/Program.cs
@@ -0,0 +1,138 @@
+﻿#define TRACK_LATENCIES
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using SpiderRock.SpiderStream;
+using SpiderRock.SpiderStream.Diagnostics;
+
+namespace TimeBoxAnalysis;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var parsed = Parse(args).ToArray();
+
+        var ifAddrArg = parsed.FirstOrDefault(arg => arg.Name == "-ifAddr");
+
+        if (ifAddrArg == null || !IPAddress.TryParse(ifAddrArg.Values[0], out var ifAddr))
+        {
+            throw new UsageException($"{nameof(TimeBoxAnalysis)}: -ifAddr must be a valid IPv4 address (i.e. 192.168.0.1) of a local NIC (ipconfig /ALL)");
+        }
+
+        var apiKey = parsed.FirstOrDefault(arg => arg.Name == "-apiKey")?.Values[0];
+
+        var logBaseDir =  @"C:\SRDiagnostics";
+        var logBaseDirArg = parsed.FirstOrDefault(args => args.Name == "-logBaseDir");
+        if(logBaseDirArg != null) {
+            logBaseDir = logBaseDirArg.Values[0];
+        }
+
+        SysEnvironment sysEnvironment = SysEnvironment.Mars;
+        var envArg = parsed.FirstOrDefault(args => args.Name == "-environment");
+        if (envArg != null)
+        {
+            switch (envArg.Values[0]) {
+                case "Mars":
+                    sysEnvironment = SysEnvironment.Mars;
+                    break;
+                case "Venus":
+                    sysEnvironment = SysEnvironment.Venus;
+                    break;
+                case "Saturn":
+                    sysEnvironment = SysEnvironment.Saturn;
+                    break;
+                default:
+                    throw new UsageException($"-environment may be one of Mars, Venus or Saturn: {envArg.Values[0]} unknown.");
+
+            }
+        }
+
+        MbusClient mbusClient = null;
+
+        try
+        {
+            mbusClient = new MbusClient(sysEnvironment)
+            {
+                LocalInterface = ifAddr,
+                ApiKey = apiKey,
+                LogBaseDirectory = logBaseDir
+            };
+
+            mbusClient.AddChannelThreadGroup("Stk[A-H]",
+                MbusChannel.StkNbboQuoteAB,
+                MbusChannel.StkNbboQuoteCD,
+                MbusChannel.StkNbboQuoteEF,
+                MbusChannel.StkNbboQuoteGH,
+                MbusChannel.StkNbboQuoteM,
+                MbusChannel.StkNbboQuoteS);
+
+
+#if TRACK_LATENCIES
+            using LatencyTracker latencyTracker = new();
+            latencyTracker.Track(mbusClient.StockBookQuote);
+#endif
+
+
+            var stockBookQuoteHandler = new StockBookQuoteHandler();
+            mbusClient.StockBookQuote.Changed += stockBookQuoteHandler.OnChange;
+
+            mbusClient.Start();
+
+            SRTrace.Process.TraceInfo($"{nameof(TimeBoxAnalysis)} running. Hit [enter] to exit.");
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine($"Engine failed to start: {e}");
+            Console.ReadLine();
+        }
+        finally
+        {
+            if (mbusClient != null)
+            {
+                mbusClient.Dispose();
+            }
+        }
+    }
+    private static IEnumerable<Arg> Parse(string[] args)
+    {
+        Arg current = null;
+
+        foreach (var arg in args)
+        {
+            if (arg.StartsWith("-"))
+            {
+                if (current != null) yield return current;
+                current = new Arg(arg);
+            }
+            else if (current != null)
+            {
+                current.Values.Add(arg);
+            }
+        }
+
+        if (current != null) yield return current;
+    }
+    private class UsageException : Exception
+    {
+        public UsageException(string message) : base(message)
+        {
+        }
+    }
+
+    private class Arg
+    {
+        public Arg(string name)
+        {
+            Name = name;
+            Values = new List<string>();
+        }
+
+        public string Name { get; private set; }
+        public List<string> Values { get; private set; }
+    }
+}
