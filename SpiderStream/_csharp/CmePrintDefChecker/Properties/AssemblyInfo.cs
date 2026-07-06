53ff0790e52677fb6789af16b1e097d2534cfbe2
“Pranav-Raman-SR” <Pranav.raman@spiderrock.net>
2026-07-05 20:32:44 -0500

    removed bad assemblies

--- SpiderStream/_csharp/CmePrintDefChecker/Properties/AssemblyInfo.cs
+++ SpiderStream/_csharp/CmePrintDefChecker/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("CmePrintDefChecker")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("CmePrintDefChecker")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- SpiderStream/_csharp/ServerExample/Properties/AssemblyInfo.cs
+++ SpiderStream/_csharp/ServerExample/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("ServerExample")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("ServerExample")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
